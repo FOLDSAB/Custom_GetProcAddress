5537421333b68b6e6e690d820a8fdfba85f9052a
Utsab Adhikari <100577045+YOURNAME116@users.noreply.github.com>
2024-05-13 16:19:44 +0545

    Create source.c

---
+++ source.c
@@ -0,0 +1,101 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+
+FARPROC cusgetprocaddress(HMODULE hmodule,LPSTR apiname);
+int main(int argc,char*argv[]) {
+
+
+	if (argc != 3) {
+		printf("please enter a valid command\n");
+		goto  END;
+	}
+	
+
+
+	LPCSTR apiname = argv[2];
+
+	
+	HMODULE hmodule = GetModuleHandleA(argv[1]);
+	
+
+
+	if (hmodule == NULL) {
+		printf("The getprocess failed with error no %d\n",GetLastError());
+	}
+
+
+	else {
+		cusgetprocaddress(hmodule,apiname);
+		FreeLibrary(hmodule);
+
+	}
+
+
+END:
+
+		printf("press enter to exit");
+		getchar();
+
+}
+
+
+FARPROC cusgetprocaddress(HMODULE hmodule,LPSTR apiname) {
+
+
+	PBYTE dllbase = (PBYTE)hmodule;
+
+
+	PIMAGE_DOS_HEADER pimgagedos = (PIMAGE_DOS_HEADER)dllbase;
+
+
+	PIMAGE_NT_HEADERS pimagent = (PIMAGE_NT_HEADERS)((DWORD_PTR)dllbase + pimgagedos->e_lfanew);
+
+
+
+	if (pimagent->Signature != IMAGE_NT_SIGNATURE) {
+		printf("failure in signature");
+		return FALSE;
+	}
+
+
+	PIMAGE_EXPORT_DIRECTORY pimageexport = (PIMAGE_EXPORT_DIRECTORY)((DWORD_PTR) dllbase + pimagent->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+
+
+
+
+	PDWORD functionnames = (PDWORD)((DWORD_PTR)dllbase + pimageexport->AddressOfNames);
+	PDWORD ordionalsaddr = (PDWORD)((DWORD_PTR)dllbase + pimageexport->AddressOfNameOrdinals);
+	PDWORD pfunctionaddresses = (PDWORD)((DWORD_PTR)dllbase + pimageexport->AddressOfFunctions);
+
+
+	for (DWORD i = 0; i < pimageexport->NumberOfNames;i++) {
+
+		LPSTR name = (LPSTR)((DWORD_PTR)pimgagedos + functionnames[i]);
+
+		WORD  ordionals = ordionalsaddr[i];
+
+		PVOID functionaddress = (PVOID)((DWORD_PTR)dllbase + pfunctionaddresses[i]);
+
+		
+
+		if (strcmp(apiname, name) == 0) {
+			printf("[%d] %s at ORDIOANL %d \t Address: %p\n", i, name, ordionals, functionaddress);
+			break;
+		}
+		else {
+			continue;
+		}
+
+		
+
+
+
+	
+	
+	}
+	
+
+}
