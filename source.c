6e1e26375f0fccdc724a67ac3e41b3c290ca5f64
FOLDSAB <100577045+FOLDSAB@users.noreply.github.com>
2024-05-16 19:39:34 +0545

    adding custom GetModuleHandle

--- source.c
+++ source.c
@@ -1,56 +1,100 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
+#include <winternl.h>
+#include <intrin.h>
 
 
-FARPROC cusgetprocaddress(HMODULE hmodule,LPSTR apiname,int *found);
-int main(int argc,char*argv[]) {
+HMODULE CustomGetmodulehandle(LPWSTR dllname);
+FARPROC cusgetprocaddress(HMODULE hmodule, LPSTR apiname, int* found);
+int main(int argc,char* argv[]) {
 
 	int found = NULL;
+
+
 	if (argc != 3) {
-		printf("please enter a valid command\n");
-		goto  END;
-	}
 	
-
-
-	LPCSTR apiname = argv[2];
-
-	
-	HMODULE hmodule = GetModuleHandleA(argv[1]);
-	
-
-
-	if (hmodule == NULL) {
-
-
-		printf("The getprocess failed with error no %d\n",GetLastError());
-	}
-
-
-	else {
-		cusgetprocaddress(hmodule,apiname,&found);
-
-		if (1 != found) {
-			printf("Cannot find the API specified\n");
-		}
-		FreeLibrary(hmodule);
+		printf("[!] Please enter full command line arguments like <DLL name> <API name>");
+		goto END;
 
 	}
+	//converting to widestring;
+	int len = MultiByteToWideChar(CP_UTF8, 0, argv[1], -1, NULL, 0);
 
+
+	LPWSTR dllname = (LPWSTR)malloc(len * sizeof(WCHAR));
+
+	
+	MultiByteToWideChar(CP_UTF8, 0, argv[1], -1, dllname, len);
+
+	
+
+	
+
+	
+
+HMODULE hmodule = 	CustomGetmodulehandle(dllname);
+
+
+if ( hmodule == NULL) {
+	wprintf(L"[!] Cannot find the desired DLL %ls\n",dllname);
+}
+
+else {
+	
+
+	LPVOID dllbase = cusgetprocaddress(hmodule, argv[2], &found);
+
+	if (found != 1) {
+		printf("[!] Cannot find requested API: %s\n", argv[2]);
+	}
+}	
+
+
+free(dllname);
 
 END:
-
-		printf("press enter to exit");
-		getchar();
+	
+	printf("\n\npress enter to exit\n");
+	getchar();
+	return 0;
 
 }
 
+HMODULE CustomGetmodulehandle(LPWSTR dllname) {
 
-FARPROC cusgetprocaddress(HMODULE hmodule,LPSTR apiname,int *found) {
+	PTEB pthreadeblock = (PTEB)__readgsqword(0x30);
+
+	PLDR_DATA_TABLE_ENTRY pdatatableentry1 = { 0 };
+	pdatatableentry1 = (PLDR_DATA_TABLE_ENTRY)pthreadeblock->ProcessEnvironmentBlock->Ldr->InMemoryOrderModuleList.Flink;
+
+	PLDR_DATA_TABLE_ENTRY pdatatableentry2 = (PLDR_DATA_TABLE_ENTRY)pthreadeblock->ProcessEnvironmentBlock->Ldr->Reserved2[1];
+
+
+	while (pdatatableentry1->FullDllName.Buffer != NULL) {
+
+		if (lstrcmpiW(dllname, (LPWSTR)pdatatableentry1->FullDllName.Buffer)==0) {
+			printf("\n\n[+] Found %ls\n", pdatatableentry1->FullDllName.Buffer);
+			printf("[+] The Dllbase is at: %p\n\n\n\n", pdatatableentry2->DllBase);
+			
+			return (HMODULE)pdatatableentry2->DllBase;
+		}
+		pdatatableentry1 = *(PLDR_DATA_TABLE_ENTRY*)pdatatableentry1;
+		pdatatableentry2 = *(PLDR_DATA_TABLE_ENTRY*)pdatatableentry2;
+
+
+	}
+
+	return NULL;
 
 	
+}
+
+
+
+FARPROC cusgetprocaddress(HMODULE hmodule, LPSTR apiname, int* found) {
+
+
 	PBYTE dllbase = (PBYTE)hmodule;
 
 
@@ -67,7 +111,7 @@ FARPROC cusgetprocaddress(HMODULE hmodule,LPSTR apiname,int *found) {
 	}
 
 
-	PIMAGE_EXPORT_DIRECTORY pimageexport = (PIMAGE_EXPORT_DIRECTORY)((DWORD_PTR) dllbase + pimagent->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+	PIMAGE_EXPORT_DIRECTORY pimageexport = (PIMAGE_EXPORT_DIRECTORY)((DWORD_PTR)dllbase + pimagent->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
 
 
 
@@ -85,25 +129,25 @@ FARPROC cusgetprocaddress(HMODULE hmodule,LPSTR apiname,int *found) {
 
 		PVOID functionaddress = (PVOID)((DWORD_PTR)dllbase + pfunctionaddresses[i]);
 
-		
+
 
 		if (strcmp(apiname, name) == 0) {
-			printf("[+] %s at \t Address: %p\n",  name, functionaddress);
+			printf("[+] %s \t Address: %p\n", name, functionaddress);
 			*found = 1;
 			break;
 		}
 		else {
-			
+
 			continue;
 		}
 
-		
 
 
 
-	
-	
+
+
+
 	}
-	
+
 
 }
